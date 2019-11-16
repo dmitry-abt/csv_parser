0ac73aad389cbd1c8d88dabff6a6525bfd15b45e
Dmitry Abt <dmitry.abt@gmail.com>
2019-11-17 02:08:47 +0300

    added compute_street stub

--- main.py
+++ main.py
@@ -14,18 +14,23 @@ def write_file(name, data):
     f.writelines(data)
 
     
+def compute_street(street):
+    result = street.replace(',','')
+    return street
+    
+    
 # main
 address = {}
 polygon = {}
 reader = csv.reader(open('live.csv'), delimiter=':')
 for row in reader:
-    key = row[2]
+    key = compute_street(row[2])
     if key not in address:
         address[key] = []
     address[key].append(row[3:])
 reader = csv.reader(open('polygon.csv'), delimiter=':')
 for row in reader:
-    key = row[4]
+    key = compute_street(row[4])
     if key not in polygon:
         polygon[key] = []
     polygon[key].append(row[0:])
