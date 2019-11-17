e18d255e991777af1cc3ae79df05631d5cc2ff8b
Dmitry Abt <dmitry.abt@gmail.com>
2019-11-18 02:02:27 +0300

    correct naming

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 
 def read_file(name):
@@ -13,36 +14,41 @@ def write_file(name, data):
     f = open(name, 'w')
     f.writelines(data)
 
-    
+
 def compute_street(street):
     quick_name = [
-        'ул.', 
-        'пр.', 
-        'пер.', 
-        'наб.', 
-        'ш.', 
-        'дор.', 
-        'бульв.', 
+        'ул.',
+        'пр.',
+        'пер.',
+        'наб.',
+        'ш.',
+        'дор.',
+        'бульв.',
         'пл.'
     ]
     full_name = [
-        'улица', 
-        'проспект', 
-        'переулок', 
-        'набережная', 
-        'шоссе', 
-        'дорога', 
-        'бульвар', 
+        'улица',
+        'проспект',
+        'переулок',
+        'набережная',
+        'шоссе',
+        'дорога',
+        'бульвар',
         'площадь'
     ]
-    street.replace(',','')
-    street.strip()
+    street = street.replace(',', '')
+    street = street.strip()
     for i in range(len(quick_name)):
         if quick_name[i] in street.lower():
             street = street.replace(quick_name[i], full_name[i])
+            if street.find(full_name[i]) < len(street) - len(full_name[i]):
+                street = street.replace(full_name[i], '')
+                street = street + ' ' + full_name[i]
+    re.sub('\s+', ' ', street).strip()
+    print (street)
     return street
-    
-    
+
+
 # main
 address = {}
 polygon = {}
@@ -58,7 +64,6 @@ for row in reader:
     if key not in polygon:
         polygon[key] = []
     polygon[key].append(row[0:])
-    
 
 # live.csv
 # polygon.csv
